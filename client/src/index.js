cdfd42c7d953c917debfe3c630e7594439459248
Nebi Resul Erkan <80630016+erkannebil@users.noreply.github.com>
2024-02-20 22:04:54 +0300

    first commit

---
+++ client/src/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+
+import './index.css';
+import App from './App';
+
+
+// Routeların çalışması için <App /> öğesini düzenlemeniz gerekir
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+    <App />
+);
